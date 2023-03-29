d6c42ded04e0fc203583aacffb6528feea0e2622
Fedois <fedefox14@gmail.com>
2023-03-29 14:19:39 +0200

    creato projectController per api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// controller
+use App\Http\Controllers\Api\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function(){
+    Route::resource('projects', ProjectController::class)->only([
+        'index', 'show'
+    ]);
 });
